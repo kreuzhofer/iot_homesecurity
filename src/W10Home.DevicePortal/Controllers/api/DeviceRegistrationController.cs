e9c5f4f7553dd4b2eca27f542af7be6a8d5d1f77
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2017-02-14 09:42:08 +0100

    add controller for device registrations

---
+++ src/W10Home.DevicePortal/Controllers/api/DeviceRegistrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using W10Home.DevicePortal.Security;
+
+namespace W10Home.DevicePortal.Controllers.api
+{
+	public class DeviceRegistrationController : ApiController
+	{
+		// POST api/<controller>
+		[ApiKeyAuthentication("cd65d126-28c4-49d0-a921-ad688d35f02a", BasicRealm = "key required")]
+		public JsonResult<RegistrationRequest> Post([FromBody]string id)
+		{
+			return Json(RegistrationRequestCache.GetNewPin(id));
+		}
+	}
+}
---
+++ src/W10Home.DevicePortal/Security/ApiKeyAuthenticationAttribute.cs
@@ -0,0 +1,47 @@
+﻿
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace W10Home.DevicePortal.Security
+{
+    public class ApiKeyAuthenticationAttribute : ActionFilterAttribute
+    {
+        public string BasicRealm { get; set; }
+        protected string ApiKey { get; set; }
+
+        public ApiKeyAuthenticationAttribute(string apikey)
+        {
+            this.ApiKey = apikey;
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+
+            var req = actionContext.Request;
+
+            if (null != req.RequestUri.Query)
+            {
+                var apikey = HttpUtility.ParseQueryString(req.RequestUri.Query).Get("api_key");
+                if (!String.IsNullOrEmpty(apikey))
+                {
+                    if (apikey == ApiKey) return;
+                }
+            }
+            actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+        }
+    }
+}
