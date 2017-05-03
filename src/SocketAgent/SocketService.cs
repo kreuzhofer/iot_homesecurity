4f5853fdf32dd66001aa6a4be1e9613ec6d473d5
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2017-05-03 21:23:17 +0200

    add packet forwarding socket agent

---
+++ src/SocketAgent/SocketService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+
+namespace SocketAgent
+{
+	public class SocketService
+	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+		private CancellationTokenSource cts;
+		private int _encodingThreads;
+
+		public SocketService(int encodingThreads)
+		{
+			_encodingThreads = encodingThreads;
+		}
+
+		public void Start()
+		{
+			logger.Debug($"Starting worker...");
+			var myThread = new Thread(new ThreadStart(Run)) { IsBackground = true };
+			myThread.Start();
+		}
+
+
+		public void Run()
+		{
+			cts = new CancellationTokenSource();
+			RunAsync(cts.Token).Wait(cts.Token);
+		}
+
+		public async Task RunAsync(CancellationToken cancellationToken)
+		{
+			while (!cancellationToken.IsCancellationRequested)
+			{
+				logger.Debug($"Starting workers...");
+				var workersList = new List<Task>();
+				for (int i = 0; i < _encodingThreads; i++)
+				{
+					var task = Task.Factory.StartNew(() => { new PacketForwadingWorker().Start(cancellationToken); }, cancellationToken);
+					workersList.Add(task);
+				}
+				Task.WaitAll(workersList.ToArray());
+			}
+			logger.Debug($"Stopped");
+		}
+
+		public void Stop()
+		{
+			logger.Debug($"Stopping");
+			this.cts.Cancel();
+			logger.Debug($"Cancellation sent");
+		}
+	}
+}
---
+++ src/W10Home.App.Shared/PacketForwardingWorker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace W10Home.Core
+{
+	internal class PacketForwardingWorker
+	{
+		private string _sourceHost;
+		private string _sourcePort;
+		private string _targetHost;
+		private string _targetPort;
+
+		public PacketForwardingWorker(string sourceHost, string sourcePort, string targetHost, string targetPort)
+		{
+			_sourceHost = sourceHost;
+			_sourcePort = sourcePort;
+			_targetHost = targetHost;
+			_targetPort = targetPort;
+		}
+
+		public async Task RunAsync(CancellationToken token)
+		{
+			do
+			{
+				token.ThrowIfCancellationRequested();
+
+				try
+				{
+					await RunAsyncInternal(token);
+				}
+				catch
+				{
+					// ignore
+				}
+				await Task.Delay(1000);
+			} while (true);
+		}
+
+		private async Task RunAsyncInternal(CancellationToken token)
+		{
+			var sourceSocket = new StreamSocket();
+			var sourceStreamReader = sourceSocket.InputStream.AsStreamForRead();
+			var sourceStreamWriter = sourceSocket.OutputStream.AsStreamForWrite();
+			await sourceSocket.ConnectAsync(new HostName(_sourceHost), _sourcePort);
+
+			var targetSocket = new StreamSocket();
+			var targetStreamReader = targetSocket.InputStream.AsStreamForRead();
+			var targetStreamWriter = targetSocket.OutputStream.AsStreamForWrite();
+			await targetSocket.ConnectAsync(new HostName(_targetHost), _targetPort);
+
+			int packetSize = 8192;
+			var buffer = new byte[packetSize];
+
+			var task1 = Task.Factory.StartNew(() =>
+			{
+				do
+				{
+					token.ThrowIfCancellationRequested();
+
+					var readBytes = sourceStreamReader.Read(buffer, 0, packetSize);
+					if (readBytes > 0)
+					{
+						targetStreamWriter.Write(buffer, 0, readBytes);
+						targetStreamWriter.Flush();
+					}
+				} while (true);
+			}, token);
+			var task2 = Task.Factory.StartNew(() =>
+			{
+				do
+				{
+					token.ThrowIfCancellationRequested();
+
+					var readBytes = targetStreamReader.Read(buffer, 0, packetSize);
+					if (readBytes > 0)
+					{
+						sourceStreamWriter.Write(buffer, 0, readBytes);
+						sourceStreamWriter.Flush();
+					}
+				} while (true);
+			}, token);
+			Task.WaitAll(new[]{task1, task2}, token);
+		}
+	}
+}
