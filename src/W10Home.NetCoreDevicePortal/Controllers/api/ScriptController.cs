9e98ea095062ea522e88b34a41a87df01eefa8a6
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2017-06-03 20:28:43 +0200

    add scriptcontroller. empty so far.

---
+++ src/W10Home.NetCoreDevicePortal/Controllers/api/ScriptController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace W10Home.NetCoreDevicePortal.Controllers.api
+{
+    [Produces("application/json")]
+    [Route("api/Script")]
+    public class ScriptController : Controller
+    {
+        // GET: api/Script
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Script/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Script
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Script/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
