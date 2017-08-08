56d3dea18904aeb1632475e3d473f720401275e7
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2017-08-08 12:31:58 +0200

    refactoring

--- src/DevicePortal/W10Home.NetCoreDevicePortal/Security/RegistrationRequestCache.cs
+++ src/DevicePortal/W10Home.NetCoreDevicePortal/Security/RegistrationRequestCache.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
-namespace W10Home.DevicePortal.Security
+namespace W10Home.NetCoreDevicePortal.Security
 {
 	public class RegistrationRequest
 	{
