0ac9e1bb9227205478cc25f3f8f9763312cb7255
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2017-09-06 14:06:27 +0200

    add limpet launcher permission to setup script

--- src/IoTCore_Setup/setup_iot.cmd
+++ src/IoTCore_Setup/setup_iot.cmd
@@ -1,1 +1,2 @@
+reg.exe ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\EmbeddedMode\ProcessLauncher" /v AllowedExecutableFilesList /t REG_MULTI_SZ /d "c:\windows\system32\limpet.exe\0"
 schtasks /Create /TN "IoTHomesecurity" /SC MINUTE /MO 1 /TR "c:\deploy\checkapp.cmd > c:\deploy\checkapp.log" /RU SYSTEM
