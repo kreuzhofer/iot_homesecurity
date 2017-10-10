48df0742e8ab496d3eae488339c369047dbbaa1b
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2017-10-10 19:39:41 +0200

    add logging controller. testing header authentication with api attribute

---
+++ src/DevicePortal/W10Home.NetCoreDevicePortal/Controllers/api/LoggingApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using W10Home.NetCoreDevicePortal.Security;
+
+namespace W10Home.NetCoreDevicePortal.Controllers.api
+{
+    [ApiKeyAuthentication("550a4f21-18e0-4467-8d72-1060825f98d0")]
+    [Produces("application/json")]
+    [Route("api/Logging")]
+    public class LoggingApiController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] LogEvent logEvent /*deviceId/*, [FromBody] string message, [FromBody] string severity*/)
+        {
+            return Json(logEvent);
+        }
+    }
+
+    public class LogEvent
+    {
+        public string DeviceId { get; set; }
+        public string Message { get; set; }
+        public string Severity { get; set; }
+    }
+}
--- src/DevicePortal/W10Home.NetCoreDevicePortal/Security/ApiKeyAuthenticationAttribute.cs
+++ src/DevicePortal/W10Home.NetCoreDevicePortal/Security/ApiKeyAuthenticationAttribute.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using System;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace W10Home.NetCoreDevicePortal.Security
 {
@@ -16,17 +20,16 @@ namespace W10Home.NetCoreDevicePortal.Security
         {
             base.OnActionExecuting(actionContext);
 
-            //var req = actionContext.ActionArguments;
-
-            //if (null != req.RequestUri.Query)
-            //{
-            //    var apikey = HttpUtility.ParseQueryString(req.RequestUri.Query).Get("api_key");
-            //    if (!String.IsNullOrEmpty(apikey))
-            //    {
-            //        if (apikey == ApiKey) return;
-            //    }
-            //}
-            //actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            if (actionContext.HttpContext.Request.Headers.All(h => h.Key.ToLower() != "apikey"))
+            {
+                actionContext.Result = new UnauthorizedResult();
+                return;
+            }
+            var apiKey = actionContext.HttpContext.Request.Headers.SingleOrDefault(h=>h.Key.ToLower() == "apikey").Value.FirstOrDefault();
+            if (String.IsNullOrEmpty(apiKey) || apiKey != this.ApiKey)
+            {
+                actionContext.Result = new UnauthorizedResult();
+            }
         }
     }
 }
