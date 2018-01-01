6316b199bdf62592688b6f6e828c6a6865ef8eb6
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2018-01-01 22:56:22 +0100

    enumerate homematic devices

--- src/IoTApp/IoTHs.Plugin.HomeMatic/HomeMaticDevice.cs
+++ src/IoTApp/IoTHs.Plugin.HomeMatic/HomeMaticDevice.cs
@@ -5,11 +5,13 @@ using IoTHs.Devices.Interfaces;
 using IoTHs.Api.Shared;
 using System;
 using System.Threading;
+using System.Xml;
 using Windows.Security.Cryptography.Certificates;
 using Windows.UI.Xaml.Automation;
 using Windows.Web.Http;
 using Windows.Web.Http.Filters;
 using IoTHs.Core;
+using IoTHs.Core.Http;
 using IoTHs.Core.Queing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -33,13 +35,29 @@ namespace IoTHs.Plugin.HomeMatic
         public override string Name => _name;
         public override string Type => _type;
 
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public override async Task InitializeAsync(DevicePluginConfigurationModel configuration)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             _connectionString = configuration.Properties["ConnectionString"];
             _name = configuration.Name;
             _type = this.GetType().Name;
+
+            // get all devices
+            var httpClient = new LocalHttpClient();
+            var resultDevices = await httpClient.Client.GetAsync(_connectionString + "devicelist.cgi");
+            if (resultDevices.IsSuccessStatusCode)
+            {
+                var xmlContent = await resultDevices.Content.ReadAsStringAsync();
+                var xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(xmlContent);
+                foreach (XmlNode deviceNode in xmlDocument.DocumentElement.ChildNodes)
+                {
+                    var deviceType = deviceNode.Attributes.Cast<XmlAttribute>().Single(n => n.Name == "device_type");
+                    var address = deviceNode.Attributes.Cast<XmlAttribute>().Single(n => n.Name == "address");
+                    var id = deviceNode.Attributes.Cast<XmlAttribute>().Single(n => n.Name == "ise_id");
+
+                    //.Value == "HM-LC-Bl1-FM")
+                }
+            }
 
             MessageReceiverLoop(_cancellationTokenSource.Token);
         }
@@ -92,11 +110,8 @@ namespace IoTHs.Plugin.HomeMatic
 
         private async Task RunProgram(string programId)
         {
-            var aHBPF = new HttpBaseProtocolFilter();
-            aHBPF.IgnorableServerCertificateErrors.Add(ChainValidationResult.Expired);
-            aHBPF.IgnorableServerCertificateErrors.Add(ChainValidationResult.Untrusted);
-            aHBPF.IgnorableServerCertificateErrors.Add(ChainValidationResult.InvalidName);
-            var functionContent = await new HttpClient(aHBPF).GetStringAsync(new Uri(_connectionString + "runprogram.cgi?program_id=" + programId));
+            var httpClient = new LocalHttpClient();
+            var functionContent = await httpClient.Client.GetStringAsync(new Uri(_connectionString + "runprogram.cgi?program_id=" + programId));
         }
     }
 }
